588dcabc6584083accde2a2435e2854ad365f842
ganeshbonda2007 <bvgsasi2024@gmail.com>
2026-06-13 22:29:59 +0530

    tested google_short_videos_api

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const { getJson } = require("serpapi")
+// getJson({
+//     engine: "google",
+//     q: "Virat Kohli",
+//     location: "India",
+//     google_domain: "google.com",
+//     hl: "en",
+//     api_key: "952d7d2b0104d7fbb67a1181e46a3afda388dad7c0fc0ba0dbca7db2c06523af"
+// }, (json) => {
+//     console.log(json)
+// })
+
+
 getJson({
-    engine: "google",
+    engine: "google_short_videos",
     q: "Virat Kohli",
     location: "India",
     google_domain: "google.com",
