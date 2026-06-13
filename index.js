3088e5e9696126c362a5ee38d2d09eb36ca4e744
ganeshbonda2007 <bvgsasi2024@gmail.com>
2026-06-13 22:38:35 +0530

    tested google_lens api

--- index.js
+++ index.js
@@ -11,13 +11,24 @@ const { getJson } = require("serpapi")
 // })
 
 
+// getJson({
+//     engine: "google_short_videos",
+//     q: "Virat Kohli",
+//     location: "India",
+//     google_domain: "google.com",
+//     hl: "en",
+//     api_key: "952d7d2b0104d7fbb67a1181e46a3afda388dad7c0fc0ba0dbca7db2c06523af"
+// }, (json) => {
+//     console.log(json)
+// })
+
 getJson({
-    engine: "google_short_videos",
-    q: "Virat Kohli",
+    engine: "google_lens",
+    url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQf84Mm_PyTmXinZoJXaw3cd2pd6vE4zzORWA&s",
     location: "India",
     google_domain: "google.com",
     hl: "en",
     api_key: "952d7d2b0104d7fbb67a1181e46a3afda388dad7c0fc0ba0dbca7db2c06523af"
 }, (json) => {
-    console.log(json)
+    console.log(json['visual_matches'])
 })
