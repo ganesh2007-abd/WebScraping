9fff92abfe311ac488034cd1f1f2e5b3a838e8b6
ganeshbonda2007 <bvgsasi2024@gmail.com>
2026-06-13 22:23:18 +0530

    tested jsonOutcome from serpapi

---
+++ index.js
@@ -0,0 +1,11 @@
+const { getJson } = require("serpapi")
+getJson({
+    engine: "google",
+    q: "Virat Kohli",
+    location: "India",
+    google_domain: "google.com",
+    hl: "en",
+    api_key: "952d7d2b0104d7fbb67a1181e46a3afda388dad7c0fc0ba0dbca7db2c06523af"
+}, (json) => {
+    console.log(json)
+})
